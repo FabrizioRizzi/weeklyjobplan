ac41170b1d3e67664d41ddc52677764497998233
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2021-04-06 23:09:13 +0200

    Added firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import Header from '../components/Header';
 
 const Home: React.FC = () => {
+
+  firebase.firestore().collection('year').get().then(a => {
+    console.log(a)
+  });
+  
   return (
     <>
       <div>Ciao sono la home</div>
