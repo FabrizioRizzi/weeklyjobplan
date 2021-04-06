c7fc68008d5c95f78ce2018c446710c878fd7725
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2021-04-06 22:14:54 +0200

    Added Home page and header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header: React.FC = () => {
+  return (
+    <div>Ciao sono l'header</div>
+  )
+};
+
+export default Header;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <div>Ciao sono la home</div>
+      <Header />
+    </>
+  )
+};
+
+export default Home;
