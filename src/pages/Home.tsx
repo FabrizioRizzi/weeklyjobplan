895470ce180a1407f61fda3dba5faad56617f46e
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2021-04-07 20:45:08 +0200

    Improved Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,16 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import Header from '../components/Header/Header';
+import './Home.css'
+
+interface Task {
+  name: string;
+  description: string;
+  dayIndex: number;
+  done: boolean;
+  week: number;
+  year: number;
+}
 
 const Home: React.FC = () => {
 
@@ -11,12 +21,13 @@ const Home: React.FC = () => {
 
   const [week, setWeek] = useState<number>(dayjs().week());
   const [loading, setLoading] = useState<boolean>(false);
+  const [tasks, setTasks] = useState<Task[]>([])
 
   useEffect(() => {
     setLoading(true);
     firebase.firestore().collection('tasks').where("year", "==", 2021).where("week", "==", week).get()
-    .then(tasks => tasks.docs.map(task => console.log(task.data())))
-    .finally(() => setLoading(false));
+      .then(tasks => setTasks(tasks.docs.map(task => task.data() as Task)))
+      .finally(() => setLoading(false));
   }, [week])
 
   const previousWeek = () => setWeek(week - 1);
@@ -25,7 +36,35 @@ const Home: React.FC = () => {
   return (
     <>
       <Header week={week} previousWeek={previousWeek} nextWeek={nextWeek} />
-      {loading && <div>Loading</div>}
+      <div className="days">
+        <div>
+          <h3>Lunedì</h3>
+        </div>
+        <div>
+          <h3>Martedì</h3>
+        </div>
+        <div>
+          <h3>Mercoledì</h3>
+        </div>
+        <div>
+          <h3>Giovedì</h3>
+        </div>
+        <div>
+          <h3>Venerdì</h3>
+        </div>
+      </div>
+      {loading ?
+      <div className="circleThing"></div> :
+        <div className="days">
+          <div></div>
+          <div></div>
+          <div>
+            {tasks.filter(task => task.dayIndex === 2).map(task => <div>{task.name}</div>)}
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      }
     </>
   )
 };
