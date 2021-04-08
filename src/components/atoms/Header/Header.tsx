83f70587002ea83c310cd3f17c1b6e3c6ea67b8f
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2021-04-08 12:34:33 +0200

    Moved components

---
+++ src/components/atoms/Header/Header.tsx
@@ -0,0 +1,25 @@
+import Button from '../Button/Button';
+import './Header.css';
+
+export interface HeaderProps {
+  week: number;
+  previousWeek: () => void;
+  nextWeek: () => void;
+  resetWeek: () => void;
+}
+
+const Header: React.FC<HeaderProps> = (props) => {
+  return (
+    <div className="header">
+      <div className="buttons">
+        <Button primary={true} title="Prima" onClick={props.previousWeek} />
+        <Button primary={true} title="Dopo" onClick={props.nextWeek} />
+        <Button primary={true} title="Reset" onClick={props.resetWeek} />
+      </div>
+      <span>Settimana {props.week || ''}</span>
+      <Button primary={true} title="Aggiungi" onClick={() => console.log('ciao')} />
+    </div>
+  )
+};
+
+export default Header;
