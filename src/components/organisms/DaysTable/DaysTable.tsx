3d71d5e806de6a37d0e47c1340c26275882d2204
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2021-04-08 12:34:43 +0200

    Added DaysTable organism

---
+++ src/components/organisms/DaysTable/DaysTable.tsx
@@ -0,0 +1,32 @@
+import dayjs from 'dayjs';
+import 'dayjs/locale/it';
+import { Task } from '../../../sharedInterfaces';
+import './DaysTable.css';
+
+dayjs.locale('it');
+
+export interface DaysTableProps {
+  week: number;
+  tasks: Task[];
+  loading: boolean;
+}
+
+const DaysTable: React.FC<DaysTableProps> = (props) => {
+  return (
+    <>
+    <div className="days">
+      {[1, 2, 3, 4, 5].map(index => {
+        return (
+          <div>
+            <h3 className="dayTitle">{dayjs().week(props.week).day(index).format('dddd DD MMMM')}</h3>
+            {!props.loading && props.tasks.filter(task => task.dayIndex === index).map(task => <div>{task.name}</div>)}
+          </div>
+        )
+      })}
+    </div>
+    { props.loading && <div className="circleThing"></div> }
+    </>
+  )
+};
+
+export default DaysTable;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,16 @@
 import dayjs from 'dayjs';
 import weekOfYear from 'dayjs/plugin/weekOfYear';
 import { useEffect, useState } from 'react';
-import Header from '../components/Header/Header';
+import Header from '../components/atoms/Header/Header';
+import DaysTable from '../components/organisms/DaysTable/DaysTable';
+import { Task } from '../sharedInterfaces';
 import { getTasksByWeek } from '../firebase/firebase';
 import './Home.css'
 
-interface Task {
-  name: string;
-  description: string;
-  dayIndex: number;
-  done: boolean;
-  week: number;
-  year: number;
-}
+
+dayjs.extend(weekOfYear);
 
 const Home: React.FC = () => {
-
-  dayjs.extend(weekOfYear);
-
   const [week, setWeek] = useState<number>(dayjs().week());
   const [loading, setLoading] = useState<boolean>(false);
   const [tasks, setTasks] = useState<Task[]>([])
@@ -34,41 +27,15 @@ const Home: React.FC = () => {
      getTasks();
   }, [week])
 
-  const previousWeek = () => setWeek(week - 1);
-  const nextWeek = () => setWeek(week + 1);
+  const previousWeek = () => setWeek(week > 1 ? week - 1 : week);
+  const nextWeek = () => setWeek(week < 52 ? week + 1 : week);
+  const resetWeek = () => setWeek(dayjs().week())
 
   return (
     <>
-      <Header week={week} previousWeek={previousWeek} nextWeek={nextWeek} />
-      <div className="days">
-        <div>
-          <h3>Lunedì</h3>
-        </div>
-        <div>
-          <h3>Martedì</h3>
-        </div>
-        <div>
-          <h3>Mercoledì</h3>
-        </div>
-        <div>
-          <h3>Giovedì</h3>
-        </div>
-        <div>
-          <h3>Venerdì</h3>
-        </div>
-      </div>
-      {loading ?
-      <div className="circleThing"></div> :
-        <div className="days">
-          <div></div>
-          <div></div>
-          <div>
-            {tasks.filter(task => task.dayIndex === 2).map(task => <div>{task.name}</div>)}
-          </div>
-          <div></div>
-          <div></div>
-        </div>
-      }
+      <Header week={week} previousWeek={previousWeek} nextWeek={nextWeek} resetWeek={resetWeek} />
+
+      <DaysTable week={week} tasks={tasks} loading={loading}/>
     </>
   )
 };
