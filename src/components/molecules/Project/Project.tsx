3d67ce9499550291f1018215b7fcf8dd473a182f
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2021-05-11 13:00:16 +0200

    Improved Project

--- src/components/molecules/Project/Project.tsx
+++ src/components/molecules/Project/Project.tsx
@@ -1,9 +1,37 @@
 import { ProjectInterface } from 'sharedInterfaces';
+import './Project.scss';
 
 export interface ProjectProps {
   project: ProjectInterface;
 }
 
-const Project: React.FC<ProjectProps> = ({ project }: ProjectProps) => (<div>{project.name}</div>);
+const Project: React.FC<ProjectProps> = ({ project }: ProjectProps) => (
+  <div className="Project__Project">
+    <div>
+      Name:
+      {project.name}
+    </div>
+    <div>
+      Description:
+      {project.description}
+    </div>
+    <div>
+      Technologies:
+      {project.technologies}
+    </div>
+    <div>
+      Deploy:
+      {project.deploy}
+    </div>
+    <div>
+      Notes:
+      {project.notes}
+    </div>
+    <div>
+      Closed:
+      {project.closed}
+    </div>
+  </div>
+);
 
 export default Project;
