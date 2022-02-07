5869e959e2caabcbe626eae8ce699981761ee8f8
FABRIZIO RIZZI <fabrizio@fabriziorizzi.it>
2022-02-07 16:37:57 +0100

    Fixed service worker

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import RootNavigation from './navigation/RootNavigation';
+import { register as registerServiceWorker } from './serviceWorkerRegistration';
+
 import './index.scss';
 
 ReactDOM.render(
@@ -15,3 +17,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+registerServiceWorker();
