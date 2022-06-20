dbb33a4eed77a1cebde806e6f701f316130b9036
Fabrizio Rizzi <fabrizio@fabriziorizzi.it>
2022-06-20 11:46:20 +0200

    Updated libraries

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'airbnb-typescript',
-  ],
+  extends: ["plugin:react/recommended", "airbnb", "airbnb-typescript", "plugin:storybook/recommended"],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/require-default-props": "off",
     "jsx-a11y/click-events-have-key-events": "off",
     "jsx-a11y/no-static-element-interactions": "off",
-    "import/no-extraneous-dependencies": [
-      "error",
-      {
-        devDependencies: [
-          "**/*.stories.tsx" 
-        ]
-      }
-    ]
-  },
+    "import/no-extraneous-dependencies": ["error", {
+      devDependencies: ["**/*.stories.tsx"]
+    }]
+  }
 };
